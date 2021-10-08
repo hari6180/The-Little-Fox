4b4cc0e5832b31634eb4ad54ba94b5d144d52756
Hari Jeon <ori080525@gmail.com>
2021-10-08 23:45:35 +0900

    add loading bar

--- src/script.js
+++ src/script.js
@@ -4,16 +4,14 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "dat.gui";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
-import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
-
 import { KeyDisplay } from "./utils";
 import { CharacterControls } from "./characterControls";
+import { gsap } from "gsap";
 
 /**
  * Loaders
  */
 const loadingBarElement = document.querySelector(".loading-bar");
-let sceneReady = false;
 const loadingManager = new THREE.LoadingManager(
   // Loaded
   () => {
@@ -26,9 +24,6 @@ const loadingManager = new THREE.LoadingManager(
       loadingBarElement.classList.add("ended");
       loadingBarElement.style.transform = "";
     }, 500);
-    window.setTimeout(() => {
-      sceneReady = true;
-    }, 2000);
   },
 
   // Progress
@@ -52,16 +47,32 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 /**
- * Sphere
+ * Overlay
  */
-// const sphere = new THREE.Mesh(
-//   new THREE.SphereGeometry(2, 32, 16),
-//   new THREE.MeshBasicMaterial({ color: 0x6699ff })
-// );
-// sphere.position.y = -2;
-// sphere.receiveShadow = true;
-// sphere.castShadow = true;
-// scene.add(sphere);
+const overlayGeometry = new THREE.PlaneGeometry(2, 2, 1, 1);
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const overlayMaterial = new THREE.ShaderMaterial({
+  transparent: true,
+  uniforms: {
+    uAlpha: { value: 1 },
+  },
+  vertexShader: `
+         void main()
+         {
+             gl_Position = vec4(position, 1.0);
+         }
+     `,
+  fragmentShader: `
+         uniform float uAlpha;
+ 
+         void main()
+         {
+             gl_FragColor = vec4(0.0, 0.0, 0.0, uAlpha);
+         }
+     `,
+});
+const overlay = new THREE.Mesh(overlayGeometry, overlayMaterial);
+scene.add(overlay);
 
 /**
  * Lights
@@ -147,7 +158,7 @@ camera.add(listener);
 const sound = new THREE.Audio(listener);
 
 // load a sound and set it as the Audio object's buffer
-const audioLoader = new THREE.AudioLoader();
+const audioLoader = new THREE.AudioLoader(loadingManager);
 audioLoader.load("music/bensound-smile.mp3", function (buffer) {
   sound.setBuffer(buffer);
   sound.setLoop(true);
@@ -158,7 +169,6 @@ audioLoader.load("music/bensound-smile.mp3", function (buffer) {
 /**
  * Particles
  */
-const textureLoader = new THREE.TextureLoader();
 const particleTexture = textureLoader.load("/textures/particles/9.png");
 
 // Geometry
@@ -195,10 +205,10 @@ scene.add(particles);
 /**
  * Model with animations
  */
-const dracoLoader = new DRACOLoader();
+const dracoLoader = new DRACOLoader(loadingManager);
 dracoLoader.setDecoderPath("/draco/");
 
-const gltfLoader = new GLTFLoader();
+const gltfLoader = new GLTFLoader(loadingManager);
 gltfLoader.setDRACOLoader(dracoLoader);
 
 let mixer = null;
